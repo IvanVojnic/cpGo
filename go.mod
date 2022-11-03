6a07199f61fe7afe54ab62226f32777b9da4f9e3
IvanVojnic <ivojnic44@gmail.com>
2022-11-03 23:30:02 +0300

    init: basic server and go mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/IvanVojnic/cpGo.git
+
+go 1.19
--- server.go
+++ server.go
@@ -1,1 +1,25 @@
 package cpGo
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string) error {
+	s.httpServer = &http.Server{
+		Addr:           ":" + port,
+		MaxHeaderBytes: 1 << 20,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
